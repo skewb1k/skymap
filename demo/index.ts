ea5ae03716906eb320039a16a4a399d981f7745f
skewb1k <101928759+skewb1k@users.noreply.github.com>
2025-01-26 02:39:10 +0300

    demo + formatting

---
+++ demo/index.ts
@@ -0,0 +1,13 @@
+// import { VirtualSky } from "../src/VirtualSky";
+
+// const canvas = document.querySelector("canvas");
+// if (!canvas) {
+// 	throw new Error("Canvas not found");
+// }
+
+// const vs = new VirtualSky({
+// 	canvas: canvas,
+// 	time: new Date(),
+// 	longitude: 0,
+// 	latitude: 0,
+// });
