0ee0e82dd336a383aadef4ad91916efd92deed9d
skewb1k <101928759+skewb1k@users.noreply.github.com>
2025-02-26 16:25:41 +0300

    feat: animations

--- src/helpers/projectSphericalTo2D.ts
+++ src/helpers/projectSphericalTo2D.ts
@@ -1,7 +1,7 @@
 import type Coo from "../types/Coo.type";
 
 export default function projectSphericalTo2D(center: Coo, alt: number, az: number, radius: number): Coo {
-	const r = (radius * (90 - alt)) / 90; // Adjust for altitude
+	const r = (radius * (90 - alt)) / 90;
 	return {
 		x: center.x + r * Math.sin(az),
 		y: center.y - r * Math.cos(az),
